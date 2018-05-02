53e3a0f359793c9974e583ddac8c49edda0a09f1
kai2june <104703027@nccu.edu.tw>
2018-05-02 20:05:30 +0800

    Initial commit(Render index.ejs)

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules":{
+        "comma-dangle": 0
+    }
+};  //0=ignored, 1=warning, 2=error
---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan('combined'));
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, './node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, './node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, './node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index',
+      {
+      title: 'NCCU course system',
+      nav: [{ link: '/course', title: 'courses' }, { link: '/curriculum', title: 'curriculums' }]
+      }
+  );
+});
+app.listen(port, () => {
+  debug(`Running on port ${chalk.green(port)}`);
+  console.log(`Running on port ${chalk.green(port)}`);
+});
