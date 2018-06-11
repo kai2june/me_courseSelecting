a11369303dcdb176dbc38f83a0f4cab4f7766c0e
kai2june <104703027@nccu.edu.tw>
2018-06-11 21:21:35 +0800

    query condition according to previous condition now.

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -5,19 +5,17 @@ const indexRouter = express.Router();
 const academy = ['-', '理學院', '文學院', '社科院', '法學院', '商學院', '外文學院', '傳播學院', '國際學院', '教育學院'];
 const degree = ['-', '學士班', '碩士班', '博士班'];
 const department = [['-'], ['資科系'], ['中文系'], ['社會系'], ['法律系'], ['金融系'], ['歐語系'], ['新聞系'], ['外交系'], ['教育系']];
-const college = {'理學院': {'學士班':['心理系', '應數系', '資科系'], '碩士班': ['心理系', '應數系', '資科系'],'博士班': ['心理系', '應數系', '資科系']}};
 
 const router = function () {
-    // indexRouter.use((req, res, next) => {
-    //     if (req.user) {
-    //         next();
-    //     } else {
-    //         res.redirect('/');
-    //     }
-    // });
+    indexRouter.use((req, res, next) => {
+        if (req.user) {
+            next();
+        } else {
+            res.redirect('/');
+        }
+    });
     indexRouter.route('/')
         .get((req, res) => {
-            console.log(college[0]);
             res.render(
                 'index',
                 {
